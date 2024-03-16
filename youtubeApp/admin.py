bdc90a592fb202adb7f549bc0f1a6b905a332d2b
jobaer <1.jobaer.ahamed@gmail.com>
2024-03-16 15:27:10 +0600

    add youtubeapp model

--- youtubeApp/admin.py
+++ youtubeApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category,Channel,VideoDetail,VideoFiles
 # Register your models here.
+
+admin.site.register(Channel)
+admin.site.register(Category)
+admin.site.register(VideoFiles)
+admin.site.register(VideoDetail)
--- youtubeApp/models.py
+++ youtubeApp/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.conf import settings
+import uuid
 # Create your models here.
 
 
 User=settings.AUTH_USER_MODEL
+
+
+def channel_directory_path(instance, filename):
+    return "video_files/channel_{0}/{1}".format(instance.channel.id, filename)
 
 class Category(models.Model):
     name = models.CharField(max_length = 30)
@@ -15,10 +20,33 @@ class Channel(models.Model):
     name = models.CharField(max_length = 150)
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     channel_art=models.ImageField(upload_to="channel/", default='default_art.jpg')
-    channel_icon=models.ImageField(upload_to='profile/', default='default_icon.png')
+    channel_icon=models.ImageField(upload_to='profile/', default='default_icon.jpg')
     slug = models.SlugField()
     description = models.TextField(blank=True, null = True)
     category = models.ForeignKey(Category, on_delete= models.CASCADE)
     
     def __str__(self):
         return self.name
+    
+
+class VideoFiles(models.Model):
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    video = models.FileField(upload_to=channel_directory_path)
+    channel = models.ForeignKey(Channel, on_delete = models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add = True)
+    likes = models.ManyToManyField(User, related_name="video_liked")
+    dislikes = models.ManyToManyField(User, related_name="video_disliked")
+    
+    def __str__(self):   
+        return f"video_file_{self.id}"
+    
+
+class VideoDetail(models.Model):
+    videofile = models.OneToOneField(VideoFiles, on_delete = models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    visibility=models.BooleanField(choices=((False, "private"),(True, "public")))
+    thumbnail=models.ImageField(upload_to="thumbnail/")
+    
+    def __str__(self):
+        return self.title
